c51cba8c536a822ae9fc53f6008ebcb901ffe91f
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-05-23 16:41:09 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('image/upload', [App\Http\Controllers\ImageUploadController::class, 'store'])->name('image.upload');
-Route::any('/get-response/{success}/{url}', [App\Http\Controllers\ImageUploadController::class, 'receive'])->name('video.response');
+Route::any('/get-response/{success}/{data}', [App\Http\Controllers\ImageUploadController::class, 'receive'])->name('video.response');
